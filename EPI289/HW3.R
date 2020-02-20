a22215b63af066f4cc8a02c0fe5a71b2036b5a42
Keikokunitoki <keikokunitoki@hsph.harvard.edu>
2020-02-20 00:48:58 -0500

    a

--- EPI289/HW3.R
+++ EPI289/HW3.R
@@ -11,7 +11,9 @@ summary(fit)
 nhefs.nmv$p.qsmk <- predict(fit, nhefs.nmv, type="response")
 
 nhefs.nmv$w <- ifelse(nhefs.nmv$qsmk==1, 1/nhefs.nmv$p.qsmk, 1/(1-nhefs.nmv$p.qsmk))
+nhefs.nmv$sw <- ifelse(nhefs.nmv$qsmk==1, nhefs$predict_nnhefs$predict_d,(1-nhefs$predict_n)/(1-nhefs$predict_d))
 summary(nhefs.nmv$w)
+summary(nhefs.nmv$sw)
 
 # marginal structural model with conservative 95% CI
 library("geepack")
@@ -21,9 +23,11 @@ summary(msm.valid)
 
 beta <- coef(msm.valid)
 SE <- coef(summary(msm.valid))[,2]
-lcl <- beta-qnorm(0.975)*SE 
+lcl <- beta-qnorm(0.975)*SE   #because of pseudo population, we have to use robust SE, bit wider than default
 ucl <- beta+qnorm(0.975)*SE
 cbind(beta, lcl, ucl)
+
+#in boot strap, we have to whole process including calcurating IP value
 
 
 #############
@@ -31,6 +35,11 @@ cbind(beta, lcl, ucl)
 #############
 table(nhefs.nmv$age[nhefs.nmv$race == 0 & nhefs.nmv$sex == 0], 
       nhefs.nmv$qsmk[nhefs.nmv$race == 0 & nhefs.nmv$sex == 0])
+q2.nosmic <-data.frame(table(
+  subset(nhefs,qsmk==0)$age,
+  subset(nhefs,qsmk==0)$sex,
+  subset(nhefs,qsmk==0)$race))
+colnames
 table(nhefs.nmv$age[nhefs.nmv$race == 0 & nhefs.nmv$sex == 1], 
       nhefs.nmv$qsmk[nhefs.nmv$race == 0 & nhefs.nmv$sex == 1])
 
@@ -45,7 +54,11 @@ summary(fit)
 nhefs$p.qsmk <- predict(fit, nhefs, type="response")
 
 nhefs$w <- ifelse(nhefs$qsmk==1, 1/nhefs$p.qsmk, 1/(1-nhefs$p.qsmk))
+#nhefs.full$sw <- ifelse(nhefs$==1, )
 summary(nhefs$w)
+
+#weighted.mean(subset(nhefs.full,qsmk==1)$death, subset(nhefs.full, qsmk==1)$w)
+#weighted.mean(subset(nhefs.full,qsmk==0)$death, subset(nhefs.full, qsmk==0)$w)
 
 # marginal structural model with conservative 95% CI
 library("geepack")
@@ -58,3 +71,7 @@ SE <- coef(summary(msm.death))[,2]
 lcl <- beta-qnorm(0.975)*SE 
 ucl <- beta+qnorm(0.975)*SE
 cbind(beta, lcl, ucl)
+
+#95% risk difference
+msm.death <- geeglm(death ~ qsmk, data=nhefs, weights=w, id=seqn,
+                    corstr="independence", family=binomial(link=independence))
