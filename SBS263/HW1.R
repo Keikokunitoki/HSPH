249cccee3de45fd9acc2d97d90f54b0dcaae4f7a
Keikokunitoki <keikokunitoki@hsph.harvard.edu>
2020-02-20 23:19:12 -0500

    f

--- SBS263/HW1.R
+++ SBS263/HW1.R
@@ -16,3 +16,15 @@ confint.default(fit3)
 fit4 <- lm(Health4~Income4, data=data)
 summary(fit4)
 confint.default(fit4)
+
+plot(x=data$Income1,y=data$Health1,xlab="Income1",ylab="Health1",main="data1",color="blue")
+abline(fit1, color="blue")
+
+plot(x=data$Income2,y=data$Health2,xlab="Income2",ylab="Health2",main="data2",color="blue")
+abline(fit2, color="blue")
+
+plot(x=data$Income3,y=data$Health3,xlab="Income3",ylab="Health3",main="data3",color="blue")
+abline(fit3, color="blue")
+
+plot(x=data$Income4,y=data$Health4,xlab="Income4",ylab="Health4",main="data4",color="blue")
+abline(fit4, color="blue")
