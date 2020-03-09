c0654252c5e39b7669f0f8978bacb9b6191bcce8
Keikokunitoki <keikokunitoki@hsph.harvard.edu>
2020-03-09 02:14:04 -0400

    a

--- EPI289/final.R
+++ EPI289/final.R
@@ -105,3 +105,116 @@ SE <- coef(summary(msm.sw))[,2]
 lcl <- beta-qnorm(0.975)*SE 
 ucl <- beta+qnorm(0.975)*SE
 cbind(beta, lcl, ucl)
+
+
+########################
+# part 4 G-estimation #
+#######################
+
+####################################################
+# G-estimation + IPW for selection bias adjustment #
+####################################################
+
+nhefs$c <- ifelse(is.na(nhefs$sbp), 1, 0)
+
+# estimation of denominator of censoring weights
+cw.denom <- glm(c==0 ~ qsmk+ sex + race + age + as.factor(education) + 
+                  smokeintensity +  as.factor(exercise) + as.factor(active) + wt71, 
+                family=binomial, data=nhefs)
+
+nhefs.c <- nhefs[which(!is.na(nhefs$sbp)),]
+nhefs.c$pd.c <- predict(cw.denom, nhefs.c, type="response")
+
+nhefs.c$wc <- 1/(nhefs.c$pd.c)
+summary(nhefs.c$wc)
+
+
+#######################################
+# G-estimation: Closed-form estimator #
+#######################################
+
+logit.est <- glm(qsmk ~ sex + race + age + as.factor(education) + 
+                   smokeintensity +  as.factor(exercise) + as.factor(active) + wt71, 
+                 family=binomial(), data=nhefs.c, weight=wc)
+summary(logit.est)
+nhefs.c$pqsmk <- predict(logit.est, nhefs.c, type = "response")
+summary(nhefs.c$pqsmk)
+
+# solve sum(w_c * H(psi) * (qsmk - E[qsmk | L]))  = 0
+# for a single psi and H(psi) = wt82_71 - psi * qsmk
+# this can be solved as psi = sum( w_c * wt82_71 * (qsmk - pqsmk)) / sum(w_c * qsmk * (qsmk - pqsmk))
+
+with(nhefs.c, sum(wc*sbp*(qsmk - pqsmk)) / sum(wc*qsmk*(qsmk - pqsmk)))  # =psi
+
+# finding the approximate 95% CI
+#install.packages("geepack")
+library("geepack")
+grid <- seq(from = 0.5,to = 3.5, by = 0.1)
+j = 0
+Hpsi.coefs <- cbind(rep(NA,length(grid)), rep(NA, length(grid)))
+colnames(Hpsi.coefs) <- c("Estimate", "p-value")
+
+for (i in grid){
+  psi = i
+  j = j+1
+  nhefs.c$Hpsi <- nhefs.c$sbp - psi * nhefs.c$qsmk 
+  
+  gest.fit <- geeglm(qsmk ~ sex + race + age + as.factor(education) + 
+                       smokeintensity +  as.factor(exercise) + as.factor(active) + wt71 + Hpsi, 
+                     family=binomial, data=nhefs.c,
+                     weights=wc, id=seqn, corstr="independence")
+  Hpsi.coefs[j,1] <- summary(gest.fit)$coefficients["Hpsi", "Estimate"]
+  Hpsi.coefs[j,2] <- summary(gest.fit)$coefficients["Hpsi", "Pr(>|W|)"]
+}
+Hpsi.coefs
+
+
+
+
+###############################
+# Part 5 outcome regression #
+##############################
+
+model <- glm(sbp ~ qsmk +sex + race + age + as.factor(education) + 
+               smokeintensity +  as.factor(exercise) + as.factor(active) + wt71 +cens, data=nhefs)
+summary(model)
+confint(model, 'qsmk', level=0.95)
+
+############################
+## Part 6 Propensity Score ##
+#############################
+
+# PS: Parametric estimation with many covariates
+fit.para <- glm(qsmk ~ sex + race + age + as.factor(education) + 
+                  smokeintensity +  as.factor(exercise) + as.factor(active) + wt71 ,
+                 data=nhefs, family=binomial())
+summary(fit.para)
+
+
+nhefs$p.qsmk <- predict(fit.para, type="response")
+head(cbind(nhefs$seqn, nhefs$sex, nhefs$age, nhefs$p.qsmk))
+
+summary(nhefs$p.qsmk)
+
+#install.packages("psych") # install package if required
+library("psych")
+
+## regression on continuous PS
+fit.ps.cont <- glm(sbp~qsmk+p.qsmk+I(p.qsmk*p.qsmk),weights=w.c, data=nhefs)
+summary(fit.ps.cont)
+confint(fit.ps.cont, 'qsmk', level=0.95)
+
+
+####################
+# Medicaid   #
+#################
+ohie <- read.csv("C:/Users/keiko/Dropbox/2020/spring/EPI289/ohie_2020.csv")
+table(ohie$lottery, ohie$medicaid)
+table(ohie$lottery)
+
+#install.packages ("sem") # install package if required
+library(sem) 
+
+model1 <- tsls(bmi ~ medicaid, ~ lottery, data = ohie)
+summary(model1)
+confint(model1) 
